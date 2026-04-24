fa59513c00bbebb8e33cd9d3fc008394d97142ef
Athena Rowe <rowea2@uci.edu>
2026-04-24 17:15:48 +0000

    Added more to blacklist for WICS and outside sites

--- scraper.py
+++ scraper.py
@@ -13,7 +13,9 @@ blacklist = {"calendar", "portal", "apply", "admin", "password", "contact", "jga
             "tribe-bar-date=203", "tribe-bar-date=204", "tribe-bar-date=205",
             "tribe-bar-date=206", "tribe-bar-date=207", "tribe-bar-date=208",
             "tribe-bar-date=209", "tribe-bar-date=21", "events/2", "events/1",
-            "outlook", "ical=", "isg", "wiki", "login", "ramesh", "study-session"} #terms in url that flag that you should not crawl them
+            "outlook", "ical=", "isg", "wiki", "login", "ramesh", "study-session",
+            "https://wics.ics.uci.edu/events", "web.archive", "twitter", "linkedin", "facebook",
+            "youtube"} #terms in url that flag that you should not crawl them
 validDomains = {"ics.uci.edu", "cs.uci.edu", "informatics.uci.edu", "stat.uci.edu"}
 token_dict = {}
 mostWords = -1
