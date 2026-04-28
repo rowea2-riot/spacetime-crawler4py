31097e061b2705df0f3252850cad1adf9029f846
Vortex <13910999+VortexOfRainbows@users.noreply.github.com>
2026-04-28 13:32:34 -0700

    completed output on worker.py end

--- crawler/worker.py
+++ crawler/worker.py
@@ -32,3 +32,6 @@ class Worker(Thread):
                 self.frontier.add_url(scraped_url)
             self.frontier.mark_url_complete(tbd_url)
             time.sleep(self.config.time_delay)
+
+        filename = f"Outputs/CompletedOutput.txt"
+        scraper.save_top_50_to_file(filename)
